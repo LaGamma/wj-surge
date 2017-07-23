042a2f90dd7a88f1292c7ed4aeb2387d2186e4b9
Nicolas LaGamma <nplagamma@email.wm.edu>
2017-07-23 03:20:21 -0400

    Add files via upload

---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+mail('nplagamma@email.wm.edu', 'This is a PHP test', 'Hi! It is you from the past! Just checking if this script works.', 'From: test@you.com')
+//mail('admiringplants@gmail.com')
+?>

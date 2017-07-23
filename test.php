46d187e4ae98abea5899b6773dbbbe6aac452671
Nicolas LaGamma <nplagamma@email.wm.edu>
2017-07-23 03:25:01 -0400

    Delete test.php

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-mail('nplagamma@email.wm.edu', 'This is a PHP test', 'Hi! It is you from the past! Just checking if this script works.', 'From: test@you.com')
-//mail('admiringplants@gmail.com')
-?>
